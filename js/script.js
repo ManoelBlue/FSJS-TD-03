231cf8c4ef637dbe8b35b0ddf24053a5cac5edbf
Manoel Blue <manoelbluee@gmail.com>
2021-01-02 11:44:54 +0100

    Create other-job-role var and hide it

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ console.log('test');
 
 //Global variables:
 const inputName = document.getElementById("name");
+const inputOtherJob = document.getElementById("other-job-role");
 
 // Highlight name input when page first loads:
 inputName.focus();
+
+//Hides the other-job-role input by default:
+inputOtherJob.style.display = "none";
